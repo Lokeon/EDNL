d40256ac90d3b18175d7cd1106911ad625573959
lokeon <kevin.lopezcala99@gmail.com>
2019-01-10 11:55:32 +0100

    GeneralTree :penguin:

---
+++ ArbolesGenerales/Practica3/main.cpp
@@ -0,0 +1,32 @@
+#include "../arbolGeneral_Din.hpp"
+#include "../agen_E-S.hpp"
+#include <iostream>
+//#include "E1.hpp"
+
+int main()
+{
+   Agen<char> A{}, B{} ,C{}  ;
+   char op;
+   //-----------------Entradas y salidas
+    cout << "desea rellenar? S/N" ;
+    cin >> op;
+    if(op == 'S')
+    {
+        cout << "****Lectura del arbol general A ****\n";
+        rellenarAgen<char>(A, '#');
+
+        std::ofstream fs("abin.dat");
+        imprimirAgen<char>(fs, A, '#');
+        fs.close();
+        cout << "\n *****arbol guardado en abin.dat\n";
+    }
+
+    //std::cout << "*****Lectura del arbol binario A desde abin.dat****\n";
+    std::ifstream fe("abin.dat");
+    rellenarAgen<char>(fe, B);
+    fe.close();
+
+    //cout << "el arbol leido de abin.dat es: " << std::endl;
+   //imprimirAgen(B);
+
+}
