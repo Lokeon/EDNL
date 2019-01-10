1bc2bd5d8e490837d8cf90e9f5c9c9084fc6c9f3
lokeon <kevin.lopezcala99@gmail.com>
2019-01-10 18:15:14 +0100

    GeneralTree :penguin:

--- ArbolesGenerales/Practica3/Ejercicio4.hpp
+++ ArbolesGenerales/Practica3/Ejercicio4.hpp
@@ -1,1 +1,54 @@
 #include "../arbolGeneral_Din.hpp"
+#include <iostream>
+
+template <typename T>
+void podar(Agen<T> &A, const T elemento)
+{
+    if (!A.arbolVacio())
+        podar_rec(A, elemento, A.raiz());
+}
+
+template <typename T>
+void podar_rec(Agen<T> &A, const T elemento, typename Agen<T>::nodo nA)
+{
+    typename Agen<T>::nodo hijo;
+    hijo = A.hijoIzqdo(nA);
+
+    if (nA != Agen<T>::NODO_NULO)
+    {
+        if (elemento == A.elemento(nA))
+        {
+            borrar(A, nA);
+            std::cout << "Poda realizada" << std::endl;
+        }
+        else
+        {
+            while (hijo != Agen<T>::NODO_NULO)
+            {
+                podar_rec(A,elemento,hijo);
+                hijo = A.hermDrcho(hijo);
+            }
+        }
+    }
+}
+
+template <typename T>
+void borrar(Agen<T> &A, typename Agen<T>::nodo nA)
+{
+    typename Agen<T>::nodo hijo;
+    hijo = A.hijoIzqdo(nA);
+
+    if (nA != Agen<T>::NODO_NULO)
+    {
+        while (hijo != Agen<T>::NODO_NULO && A.hijoIzqdo(hijo) == Agen<T>::NODO_NULO)
+        {
+            A.eliminarHijoIzqdo(nA);
+            hijo = A.hijoIzqdo(nA);
+        }
+        if (hijo != Agen<T>::NODO_NULO && A.hijoIzqdo(hijo) != Agen<T>::NODO_NULO)
+        {
+            borrar(A, hijo);
+            borrar(A, nA);
+        }
+    }
+}
